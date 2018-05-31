8e951669b2ef0e026296bb5b16b4b5ce95357b51
YunazGilang <yunaz.gilang@gmail.com>
2018-05-31 20:58:29 +0700

    editing product details, adding randomcolor for dynamic background

--- saleor/static/js/components/image-zoomer.js
+++ saleor/static/js/components/image-zoomer.js
@@ -2,10 +2,11 @@ import "ez-plus"
 import "lightgallery"
 import "lg-thumbnail"
 import "lg-fullscreen"
-
-alert("outside")
+var randomColor = require('randomcolor'); 
 
 $(function() {
+	var $count = 0;
+	var $custom_item = $("#lightgallery");
     $('.carousel-item').each(function(i, obj) {
 	    //test
 	    $('#image-product-active-'+i).ezPlus({
@@ -17,12 +18,24 @@ $(function() {
 			responsive: true
 		}); 
 		console.log(i);
+		$count++;
+	});	
+
+    var baseColor = randomColor({hue:'blue'});
+    var color = randomColor({'count':$count,luminosity: 'dark',format: 'rgba',hue:baseColor,alpha:0.3});
+
+    console.log(color);
+	$custom_item.lightGallery({
+		thumbnail:true,
+		animateThumb: false,
+   		showThumbByDefault: false,
+		mode: 'lg-fade'
 	});
 
-    console.log('loaded')
-	
+	$custom_item.on('onBeforeSlide.lg', function(event, prevIndex, index){
+	    $('.lg-outer').css('background-color', color[index])
+	});
+
+	$('.lg-outer').css({"transition": "background-color 600ms ease 0s"});
 });
 
-$("#lightgallery").lightGallery({
-		thumbnail:true
-	});
